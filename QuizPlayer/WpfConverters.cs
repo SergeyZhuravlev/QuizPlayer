f7ee9b542487c455fe97a56fc7cd3d85ef5421d4
Sergey Zhuravlev <Sergey4Zhuravlev@gmail.com>
2022-02-08 06:04:29 +0300

    converters

---
+++ QuizPlayer/WpfConverters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace QuizPlayer
+{
+  [ValueConversion(typeof(bool), typeof(bool))]
+  public class InverseBooleanConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (targetType != typeof(bool))
+        throw new InvalidOperationException("The target must be a nullable boolean");
+      return !(bool)value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return !(bool)value;
+    }
+  }
+
+  [ValueConversion(typeof(Answer), typeof(SolidColorBrush))]
+  class AnswerToColorBrushConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value is Answer answer)
+      {
+        if (!answer.UserAnswered)
+          return new SolidColorBrush(Colors.Transparent);
+        return answer.UserRightAnswered ? new SolidColorBrush(Colors.Green) : new SolidColorBrush(Colors.Red);
+      }
+
+      return Binding.DoNothing;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
